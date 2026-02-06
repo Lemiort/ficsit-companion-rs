f7cdaefd8fd5767aec7d4a6157c3797df52fe4ed
Lemiort <lemiort@gmail.com>
2026-02-06 17:52:29 +0100

    Fix name

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Ficsit Companion RS",
         native_options,
         Box::new(|cc| Ok(Box::new(ficsit_companion_rs::TemplateApp::new(cc)))),
     )
